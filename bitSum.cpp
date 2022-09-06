fff1cc2836e6e7c8c90214df0b8d3a08b689e699
Eshan Chatterjee <eshan2001221@gmail.com>
2022-09-06 13:06:23 +0530

    Done BitSum

---
+++ bitSum.cpp
@@ -0,0 +1,26 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+unsigned int SumOfSetBits(int n, int set)
+{
+    int sum = 0;
+    for(int i = 0; i < (pow(n, 2) -1); ++i)
+    {
+        if(bitset<1024>(i).count() == set)
+        {
+            sum += i;
+        }
+    }
+
+    return sum;
+}
+
+
+int main()
+{
+    print(SumOfSetBits(3, 2) == 14);
+    print(SumOfSetBits(4, 1) == 15);
+
+    return 0;
+}
