c5b945feae469ce2a72ba402be6fd921aaca584b
Eshan Chatterjee <eshan2001221@gmail.com>
2022-09-18 15:28:17 +0530

    done another

--- stringPllay.cpp
+++ stringPllay.cpp
@@ -4,23 +4,23 @@ using namespace std;
 
 string play(string str)
 {
-    istringstream ss(str);
-    string ssa = "";
-    string s;
-    int len;
-    while(getline(ss, s, ' '))
-    {
-        reverse(s.begin(), s.end());
-        s[0] = toupper(s[0]);
-        ssa += s + " ";
-    }
+	istringstream ss(str);
+	string ssa = "";
+	string s;
+	int len;
+	while(getline(ss, s, ' '))
+	{
+		reverse(s.begin(), s.end());
+		s[0] = toupper(s[0]);
+		ssa += s + " ";
+	}
 
-    return ssa;
+	return ssa;
 }
 
 int main()
 {
-    string str = "my nAmE iS JhOn";
+	string str = "my nAmE iS JhOn";
 
-    print(play(str));
+	print(play(str));
 }
