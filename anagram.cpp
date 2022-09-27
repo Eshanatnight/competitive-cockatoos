63640e9d9bd01a82a39a141ff698518dc1795aa5
Eshan Chatterjee <eshan2001221@gmail.com>
2022-09-27 15:38:52 +0530

    Update

--- anagram.cpp
+++ anagram.cpp
@@ -17,7 +17,27 @@ string isAnnagram(string s1, string s2)
 	return "no";
 }
 
+bool isAnnagram_2(string s1, string s2)
+{
+	array<int, 26> arr;
+	arr.fill(0);
+
+	transform(s1.begin(), s1.end(), s1.begin(), [](char a) { return tolower(a); });
+	transform(s2.begin(), s2.end(), s2.begin(), [](char a) { return tolower(a); });
+
+	for(int i = 0; i < s1.length(); i++)
+	{
+		arr[s1[i] - 'a']++;
+		arr[s2[i] - 'a']--;
+	}
+
+	if(all_of(arr.begin(), arr.end(),[](int ch){ return ch != 0;}))
+		return false;
+
+	return true;
+}
+
 int main()
 {
-	print(isAnnagram("Listen", "Silent") == "yes");
+	print(isAnnagram_2("Listen", "Silent"));
 }
