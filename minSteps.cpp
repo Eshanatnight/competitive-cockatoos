f57e5e06bda7cf0242ce73b5089cbbc1cf977e14
Eshan Chatterjee <eshan2001221@gmail.com>
2022-11-07 17:15:12 +0530

    MinSteps ADDED

---
+++ minSteps.cpp
@@ -0,0 +1,40 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+int minSteps(int input1, int input2)
+{
+    int cnt(0);
+
+    while(input1 != 0 && input2 != 0)
+    {
+        if(input1 > input2)
+        {
+            cnt += input1 / input2;
+            input1 %= input2;
+        }
+
+        else
+        {
+            cnt += input2 / input1;
+            input2 %= input1;
+        }
+    }
+
+    cnt--;
+
+    if(input1 > 1 || input2 > 1)
+        return -1;
+
+    return cnt;
+}
+
+
+int main()
+{
+    int input1(2), input2(2);
+
+    cout << minSteps(input1, input2) << endl;
+
+    return 0;
+}
